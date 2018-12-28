456b99baa67d700c82e8e1b900f20a243479791e
zishanrashid <zishanr027@gmail.com>
2018-12-28 22:02:18 +0530

    About Stream in Dart

---
+++ bin/main.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+class Cake {}
+
+// Order  - Order given by customer.
+class Order {
+  String type;
+
+  Order(this.type);
+}
+
+void main() {
+  final StreamController controller = new StreamController();
+
+  final Order orderObject = new Order('chocolate');
+
+  // Named constructor StreamTransformer.fromHandlers()
+  final StreamTransformer baker =
+      new StreamTransformer.fromHandlers(handleData: (cakeType, sink) {
+    if (cakeType == 'chocolate') {
+      sink.add(new Cake());
+    } else {
+      sink.addError('I cant bake this cake');
+    }
+  });
+
+  // Sink - Ordertaker outside the factory.
+  controller.sink.add(orderObject);
+
+  // OrderInpector receives every order object passed by OrderTaker to the factory and pass the type from order object to baker.
+  controller.stream.map((order) => order.type).transform(baker).listen(
+      (cake) => print('Heres your cake $cake'),
+      onError: (error) => print('Error $error'));
+}
